906de20bdd03afd60a03b90887a19fb2302d8ace
Liam Reeves <wesleyliam005@gmail.com>
2024-06-14 18:14:36 +0300

    added POST method on forms

--- app/views.py
+++ app/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-
+from django import forms
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
+class Details(forms.Form):
+    name = forms.CharField(max_length=100, label="Name")
+    email =forms.EmailField(max_length=100, label="Email")
+    phone = forms.CharField(min_length=1, max_length=8, label="Phone")
+    address = forms.CharField(max_length=100, label="Address")
+    city = forms.CharField(max_length=100, label="City")
+    state = forms.CharField(max_length=100, label="State")
+    country = forms.CharField(max_length=100, label="Country")
+
+ 
+ 
 def index(request):
-    return render(request, "index.html")
+
+    if request.method == "POST":
+        form = Details(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponse("<h1>Form Submitted</h1>")
+        else:
+               return render(request, "index.html", {
+        "form": form
+    })
+            
+    return render(request, "index.html", {
+        "form": Details()
+    })
